d5ff2e8a907a366ec1276cf4ee6946844df6d296
Yada <133637312+mryadanigu@users.noreply.github.com>
2023-06-20 12:49:36 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,14 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
  * main - Entry point
- * Description: prints '_putchar\n'
- * Return: Always 0 (success)
+ *
+ * Description: Prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char str[] = "_putchar";
-int i = 0;
-
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
-return (0);
+	write(STDOUT_FILENO, "_putchar\n", 9);
+	return (0);
 }
