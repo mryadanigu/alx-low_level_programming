054627360862e21fcced871547c530b796a5a964
Yada <133637312+mryadanigu@users.noreply.github.com>
2023-06-20 12:55:56 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
+/**
+ * main - displays "_putchar"
+ * Return: returns 0 (success)
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    return 0;
+	return (0);
 }
