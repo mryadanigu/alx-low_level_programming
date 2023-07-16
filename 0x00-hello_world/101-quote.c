26d757c8d085c1f2d1d31e25bcd032bfc770f709
Yada <133637312+mryadanigu@users.noreply.github.com>
2023-07-16 21:45:49 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 1 (Error)
+ */
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, sizeof(message) - 1);
+    ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+    ssize_t written = write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+
+    if (written != len)
+        return 1;
     return 1;
 }
