09a3bfa94598fdf0c08bc41dace8f7b0b60044fe
Yada <133637312+mryadanigu@users.noreply.github.com>
2023-07-16 21:49:14 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,8 @@
+#include <stdio.h>
 #include <unistd.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 1 (Error)
- */
 int main(void)
 {
-	ssize_t len, written;
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	for (len = 0; message[len] != '\0'; len++)
-		;
-
-	written = write(2, message, len);
-	if (written != len)
-		return 1;
-
-	return 1;
+    write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return 1;
 }
