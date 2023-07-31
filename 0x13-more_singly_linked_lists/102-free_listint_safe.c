7b347e1bc9355690ade78662fd050c1c61b197d6
Yada <133637312+mryadanigu@users.noreply.github.com>
2023-07-31 20:11:17 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,34 +1,41 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * free_listint_safe - Frees a linked list safely to avoid infinite loops.
+ * @h: Pointer to the first node in the linked list.
+ *
+ * Return: Number of elements in the freed list.
  */
-typedef struct listint_s
+size_t free_listint_safe(listint_t **h)
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	size_t len = 0;
+	int diff;
+	listint_t *temp;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head);
+	if (!h || !*h)
+		return (0);
 
-#endif
+	while (*h)
+	{
+		/* Calculate the difference */
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
